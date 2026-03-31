bfca1ffb42071f2c1c30dbcf089e5ac1dd4c48ee
Kye-Sta <kye.stanley@my.jcu.edu.au>
2026-03-31 19:05:20 +1000

    Added Test Code for motor's step and swindle

--- main.c
+++ main.c
@@ -18,16 +18,31 @@ int main(void)
   stdio_init_all();
   mmhal_init();
 
+  mmhal_set_spindle_pwm(128);
+  sleep_ms(1000);
+  mmhal_set_spindle_pwm(0);
+
   while (true)
   {
+    mmhal_step_motors(1, 0, 0); // Step X axis in positive direction
+    sleep_ms(200);
+    mmhal_step_motors(-1, 0, 0);// Step X axis in negative direction
+    sleep_ms(200);
+    mmhal_step_motors(0, 1, 0); // Step Y axis in positive direction
+    sleep_ms(200);
+    mmhal_step_motors(0, -1, 0); // Step Y axis in negative direction
+    sleep_ms(200);
+    
     //  Repeated code here
-    if (manual_mode)
-    {
+    //if (manual_mode)
+    //{
       // handle_manual_mode();
-    }
-    else
-    {
+    //}
+    //else
+    //{
       // handle_command_mode();
-    }
+    //}
   }
+
+  return 0;
 }
